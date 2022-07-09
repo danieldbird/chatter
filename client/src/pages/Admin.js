faf3134ba842711e4016bd32919ab0bef018e83c
Daniel Bird <danieldbird@gmail.com>
2022-07-09 16:06:12 +1200

    add uuid for random room id on the client side

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -5,7 +5,9 @@ import socketClient from "socket.io-client";
 export default function Admin() {
   const [socket, setSocket] = useState(null);
   const [rooms, setRooms] = useState();
-  const [joinedRoom, setJoinedRoom] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const socket = socketClient("http://localhost:3001");
@@ -18,17 +20,34 @@ export default function Admin() {
     };
   }, []);
 
+  useEffect(() => {
+    const socket = socketClient("http://localhost:3001");
+    setSocket(socket);
+    socket.on("message", (msg) => {
+      setMessages((messages) => [...messages, msg]);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   function joinRoom(room) {
-    setJoinedRoom(room);
+    setRoomId(room);
     socket.emit("adminJoinRoom", room);
   }
 
   function leaveRoom() {
-    setJoinedRoom("");
+    setRoomId("");
     socket.emit("leaveRoom");
   }
 
   // TODO: identify if admin is in a room, so two admins are not talking to each other.
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    socket.emit("sendAdminMessage", message, roomId, () => setMessage(""));
+    setMessage("");
+  }
 
   return (
     <div className="h-full">
@@ -39,35 +58,63 @@ export default function Admin() {
               return room.members < 2 ? (
                 <li
                   key={room.name}
-                  className="my-1 rounded-md px-3 py-1 cursor-pointer bg-blue-200"
+                  className="my-1 rounded-md px-2 py-1 cursor-pointer bg-blue-200"
                   onClick={() => {
                     joinRoom(room.name);
                   }}
                 >
-                  Room {room.name}
+                  Room {room.name.slice(0, 4)}
                 </li>
               ) : (
                 <li
                   key={room.name}
-                  className="my-1 rounded-md px-3 py-1 cursor-pointer bg-gray-200"
+                  className="my-1 rounded-md px-2 py-1 cursor-pointer bg-gray-200"
                 >
-                  Room {room.name}
+                  Room {room.name.slice(0, 4)}
                 </li>
               );
             })}
-          {joinedRoom && (
+          {roomId && (
             <button
-              className="my-1 bg-red-200 rounded-md px-3 py-1 cursor-pointer"
+              className="my-1 bg-red-200 rounded-md px-2 py-1 cursor-pointer"
               onClick={leaveRoom}
             >
-              Leave Room {joinedRoom}
+              Leave Room {roomId.slice(0, 4)}
             </button>
           )}
         </ul>
         <div className="bg-gray-50 w-full flex flex-col">
-          {joinedRoom && <p>Joined Room {joinedRoom}</p>}
-          <div className="mt-auto p-5">This is where the conversation happens</div>
-          <input type="text" className="bg-gray-300 p-2" />
+          <div className="mt-auto px-8 py-6">
+            {messages.length > 0 ? (
+              messages.map((msg, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={msg.type === "client" ? "text-green-500" : "text-blue-500"}
+                  >
+                    {msg.text}
+                  </div>
+                );
+              })
+            ) : (
+              <div>No messages</div>
+            )}
+          </div>
+          <div className="bg-gray-100 flex flex-col">
+            <form
+              className="flex flex-col"
+              onSubmit={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              <input
+                type="text"
+                className="p-2 mb-2"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 
 import socketClient from "socket.io-client";
-// import Chat from "../components/chat/ChatButton";
-
+import { v4 as uuidv4 } from "uuid";
 function App() {
   const [socketConnected, setSocketConnected] = useState(false);
   const [socket, setSocket] = useState(null);
   const [socketId, setSocketId] = useState("");
+  const [roomId, setRoomId] = useState(uuidv4());
+
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   function joinRoom() {
     setSocketConnected(true);
     setSocketId(socket.id);
-    socket.emit("clientJoinRoom");
+    socket.emit("clientJoinRoom", roomId);
   }
 
   function leaveRoom() {
@@ -23,30 +26,67 @@ function App() {
   useEffect(() => {
     const socket = socketClient("http://localhost:3001");
     setSocket(socket);
+    socket.on("message", (msg) => {
+      setMessages((messages) => [...messages, msg]);
+    });
     return () => {
       socket.disconnect();
     };
   }, []);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    socket.emit("sendClientMessage", message, roomId, () => setMessage(""));
+    setMessage("");
+  }
+
   return (
     <>
-      <h1>Home page</h1>
-      <p>Socket Id: {socketId}</p>
-      {/* <Chat /> */}
-      {!socketConnected ? (
-        <button className="bg-blue-400 text-white px-3 py-2 rounded" onClick={joinRoom}>
-          Ask us a question
-        </button>
-      ) : (
-        <button
-          className="bg-red-400 text-white px-3 py-2 rounded"
-          onClick={() => {
-            leaveRoom(socket);
-          }}
-        >
-          Disconnect
-        </button>
-      )}
+      <div className="flex h-full flex-col py-1">
+        <div className=" bg-gray-100 grow flex justify-end flex-col px-12 py-6">
+          {messages.length > 0 ? (
+            messages.map((msg, i) => {
+              return (
+                <div key={i} className={msg.type === "admin" ? "text-green-500" : "text-blue-500"}>
+                  {msg.text}
+                </div>
+              );
+            })
+          ) : (
+            <div>No messages</div>
+          )}
+        </div>
+        <div className="bg-gray-100">
+          <form
+            className="flex flex-col"
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <input
+              type="text"
+              className="p-2 mx-6 mb-2"
+              placeholder="How can we help you?"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </form>
+        </div>
+        {!socketConnected ? (
+          <button className="bg-green-400 text-white px-3 py-2 rounded" onClick={joinRoom}>
+            Ask us a question
+          </button>
+        ) : (
+          <button
+            className="bg-red-400 text-white px-3 py-2 rounded"
+            onClick={() => {
+              leaveRoom(socket);
+            }}
+          >
+            Disconnect
+          </button>
+        )}
+      </div>
     </>
   );
 }
